d57de381e01780b148a50950d1009a4225709c92
Omair <omairraza9@gmail.com>
2017-10-05 16:25:30 -0400

    First commit

---
+++ crm.rb
@@ -0,0 +1,25 @@
+require_relative 'contact'
+require 'sinatra'
+
+get '/' do
+
+
+  erb :index
+end
+
+get '/contacts' do
+  @contacts = Contact.all
+  erb :contacts
+end
+
+
+after do
+  ActiveRecord::Base.connection.close
+end
+
+#irb
+# Contact.create(first_name: "Omair", last_name: "Raza", email: "o.com", note: "great")
+# Contact.create(first_name: "jim", last_name: "Halpert", email: "jh@dundermifflinINFINITY.com", note: "second-second life")
+# load "contact.rb"
+#to show -> I can do Contact.all
+puts Contact.all.inspect
