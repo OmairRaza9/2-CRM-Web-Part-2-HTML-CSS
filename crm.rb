57a8a2fe41eede805306c00333f01021011e013b
Omair <omairraza9@gmail.com>
2017-10-09 17:49:34 -0400

    second try

--- crm.rb
+++ crm.rb
@@ -2,13 +2,12 @@ require_relative 'contact'
 require 'sinatra'
 
 get '/' do
-
-
   erb :index
 end
 
 get '/contacts' do
   @contacts = Contact.all
+
   erb :contacts
 end
 
@@ -19,20 +18,24 @@ get '/contacts/:id' do
   # instructions for how to handle requests to this route will go here
     if @contact
   erb :show_contact
-else
-  raise Sinatra::NotFound
-end
+  else
+  #  raise Sinatra::NotFound
+  end
 end
 
+get '/contacts/new' do
+   #"Add a new contact"
 
+   erb :new
+end
+
+post '/contacts' do
+  puts params
+end
 
 after do
   ActiveRecord::Base.connection.close
 end
 
-#irb
-# Contact.create(first_name: "Omair", last_name: "Raza", email: "o.com", note: "great")
-# Contact.create(first_name: "jim", last_name: "Halpert", email: "jh@dundermifflinINFINITY.com", note: "second-second life")
-# load "contact.rb"
-#to show -> I can do Contact.all
-puts Contact.all.inspect
+
+#puts Contact.all.inspect
